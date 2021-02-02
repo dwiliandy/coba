5fd929b507dc088b04821056420cf2c0674ffda4
Omega Sekeon <dwiliandy@gmail.com>
2021-02-02 18:31:20 +0800

    tambah login screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   match "/404", to: "errors#not_found", via: :all
   match "/500", to: "errors#internal_server_error", via: :all
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
