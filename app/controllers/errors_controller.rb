f7398f346484197ee9617b0f00c53b99c71c57b2
Omega Sekeon <dwiliandy@gmail.com>
2021-02-02 15:17:39 +0800

    add error view

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,15 +1,10 @@
 class ErrorsController < ApplicationController
-  after_action: "save_my_previous_url"
+  
   def not_found
     render status: 404
   end
 
   def internal_server_error
     render status: 500
-  end
-
-  def save_my_previous_url
-    # session[:previous_url] is a Rails built-in variable to save last url.
-    session[:my_previous_url] = URI(request.referer || '').path
   end
 end
