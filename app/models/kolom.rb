05ac0eda11300815f1f4c49c87d3ae6e077b23c2
Omega Sekeon <dwiliandy@gmail.com>
2021-08-03 11:54:34 +0800

    tambah data jumlah jemaat

--- app/models/kolom.rb
+++ app/models/kolom.rb
@@ -8,4 +8,16 @@
 #  updated_at :datetime         not null
 #
 class Kolom < ApplicationRecord
+
+  def jumlah_nama_keluarga
+    NamaKeluarga.where("active = ? and kolom = ?",true, self.nama).count
+  end
+
+  def jumlah_anggota_keluarga
+    jumlah = 0
+    NamaKeluarga.where("active = ? and kolom = ?",true, self.nama).each do |jumlah_anggota|
+        jumlah = jumlah + jumlah_anggota.anggota_keluargas.where(meninggal: false).count
+    end
+    jumlah
+  end
 end
